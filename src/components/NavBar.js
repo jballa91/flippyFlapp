e109e3306933ee2c547531f33b8733b4baa31127
James Ballard <jamesballard901@gmail.com>
2020-06-09 16:03:44 -0500

    implement user auth

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useAuth0 } from "../flippy-flapp-spa";
+
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  return (
+    <div>
+      {!isAuthenticated && (
+        <button onClick={() => loginWithRedirect({})}>Log in</button>
+      )}
+
+      {isAuthenticated && <button onClick={() => logout()}>Log Out</button>}
+
+      {isAuthenticated && (
+        <span>
+          <Link to="/">Home</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/external-api">External API</Link>
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
