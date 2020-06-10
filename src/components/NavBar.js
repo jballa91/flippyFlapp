0f4e535bd1899682ba7be9ece4570780a33099fe
James Ballard <jamesballard901@gmail.com>
2020-06-10 17:51:52 -0500

    delete styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,77 +1,59 @@
 import React from "react";
 import { useAuth0 } from "../flippy-flapp-spa";
-// import { makeStyles } from "@material-ui/core/styles";
-// import {
-//   AppBar,
-//   Toolbar,
-//   Typography,
-//   Button,
-//   Breadcrumbs,
-// } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Breadcrumbs,
+} from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 // Import styles
-import styles from "../styles/navbar/Navbar.module.css";
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-//   menuButton: {
-//     marginRight: theme.spacing(2),
-//   },
-//   title: {
-//     flexGrow: 1,
-//     marginRight: theme.spacing(2),
-//   },
-// }));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
-  // const classes = useStyles();
+  const classes = useStyles();
 
   return (
-    <div className={styles.navbar}>
-      <Link to="/" className={styles.navbar__left}>
-        <h1 className={styles.navbar__title}>flippyFlapp</h1>
-      </Link>
-      <div className={styles.navbar__right}>
-        {isAuthenticated && (
-          <span className={styles.navbar__navlist}>
-            <Link className={styles.navbar__navlink} to="/">
-              Home
-            </Link>
-            <Link className={styles.navbar__navlink} to="/profile">
-              Profile
-            </Link>
-            <Link className={styles.navbar__navlink} to="/external-api">
-              External API
-            </Link>
-            <Link className={styles.navbar__navlink} to="/dashboard">
-              Dashboard
-            </Link>
-          </span>
-        )}
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Link to="/">
+          <h1>flippyFlapp</h1>
+        </Link>
         <div>
-          {!isAuthenticated && (
-            <button
-              className={styles.navbar__log_button}
-              onClick={() => loginWithRedirect({})}
-            >
-              Log in
-            </button>
-          )}
           {isAuthenticated && (
-            <button
-              className={styles.navbar__log_button}
-              onClick={() => logout()}
-            >
-              Log Out
-            </button>
+            <span>
+              <Link to="/">Home</Link>
+              <Link to="/profile">Profile</Link>
+              <Link to="/external-api">External API</Link>
+              <Link to="/dashboard">Dashboard</Link>
+            </span>
           )}
+          <div>
+            {!isAuthenticated && (
+              <button onClick={() => loginWithRedirect({})}>Log in</button>
+            )}
+            {isAuthenticated && (
+              <button onClick={() => logout()}>Log Out</button>
+            )}
+          </div>
         </div>
-      </div>
+      </AppBar>
     </div>
   );
 };
--- src/components/dashboard/customMap.js
+++ src/components/dashboard/customMap.js
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
-import React, { useEffect, useRef, useState } from "react";
-let places = [
-  { name: "Panda-Sycamore", lat: 32.62669, lng: -97.37 },
-  { name: "Panda-Hulen", lat: 32.71106, lng: -97.38634 },
-  { name: "Panda-TrinityCommons", lat: 32.68009, lng: -97.46404 },
-  { name: "Panda-Benbrook", lat: 32.6798922, lng: -97.4635325 },
-  { name: "Panda-Burleson", lat: 32.7134158, lng: -97.2831792 },
-];
-var flightPlanCoordinates = [
-  { lat: 37.772, lng: -122.214 },
-  { lat: 21.291, lng: -157.821 },
-  { lat: -18.142, lng: 178.431 },
-  { lat: -27.467, lng: 153.027 },
-];
-=======
 import React, { useEffect, useRef, useState } from 'react';
 import { connect } from 'react-redux'
 import { thunks as AiportStore } from '../../store/airports';
 import { thunks as FlightPathStore } from '../../store/flightPath';
 
->>>>>>> master
 // Variables
 let key = "AIzaSyDscju6O6knNTt9zh71EQkt7Lk1XeejhyQ";
 const myLocation = {
@@ -33,112 +16,6 @@ const mapStyles = {
   height: "400px",
 };
 
-<<<<<<< HEAD
-function GoogleMaps(props) {
-  // refs
-  const googleMapRef = React.createRef();
-  const googleMap = useRef(null);
-  const marker = useRef(null);
-  const [airports, setAirports] = useState([]);
-  const [flightPath, setFlightPath] = useState([]);
-
-  // helper functions
-  const createGoogleMap = () =>
-    new window.google.maps.Map(googleMapRef.current, {
-      zoom: 14,
-      center: {
-        lat: myLocation.lat,
-        lng: myLocation.lng,
-      },
-    });
-
-  const createMarker = () => {
-    //create clusterer obj
-    // var clusterer = new window.Clusterer(googleMap);
-
-    console.log(airports.length);
-    let markers = airports.map((airport) => {
-      return new window.google.maps.Marker({
-        position: {
-          lat: parseFloat(airport.lat),
-          lng: parseFloat(airport.lng),
-        },
-        map: googleMap.current,
-      });
-    });
-    console.log(markers);
-    var markerCluster = new window.MarkerClusterer(googleMap.current, markers, {
-      imagePath:
-        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-    });
-    console.log(markerCluster);
-  };
-
-  async function getAirportCoords() {
-    const airportData = await fetch("http://localhost:5000/airports/coords");
-    const { data } = await airportData.json();
-
-    const revisedlist = data.map((airport) => {
-      console.log("airport", airport);
-      return {
-        lat: parseFloat(airport.lat),
-        lng: parseFloat(airport.lng),
-        id: airport.id,
-      };
-    });
-
-    setAirports(revisedlist);
-    console.log(revisedlist);
-    console.log(airports);
-    //change later ----------------------------------------------------
-    setFlightPath([
-      { lat: 33.74772222164236, lng: -115.32524999993736 },
-      { lng: -115.32524999993736, lat: 33.74772222164236 },
-      { lat: 35.65874127804852, lng: -117.82953249991486 },
-      { lat: 38.03041666684228, lng: -120.41455555606944 },
-      { lat: 38.4400000002395, lng: -120.88694444435187 },
-
-      // { lat: 27.074498333081777, lng: -81.58646666706443 },
-      // { lat: 32.46383333285225, lng: -87.95405555602503 },
-      // { lat: 37.09399999968052, lng: -95.57200000004723 },
-      // { lat: 40.614964639338744, lng: -103.26428550007884 },
-      // { lat: 44.16236111118097, lng: -112.22066666743004 },
-      // { lat: 46.374500000137566, lng: -117.01538888899674 }
-    ]);
-  }
-
-  // useEffect Hook
-  useEffect(() => {
-    if (airports.length > 0) {
-      createMarker();
-      if (flightPath.length > 0) {
-        let flightPathPoly = new window.google.maps.Polyline({
-          path: flightPath,
-          geodesic: true,
-          strokeColor: "#FF0000",
-          strokeOpacity: 1.0,
-          strokeWeight: 2,
-        });
-        console.log();
-        flightPathPoly.setMap(googleMap.current);
-      }
-    } else if (airports.length == 0) {
-      const googleMapScript = document.createElement("script");
-      googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${key}&libraries=places`;
-      window.document.body.appendChild(googleMapScript);
-
-      //clusterer add on
-      const clustererScript = document.createElement("script");
-      clustererScript.src =
-        "https://unpkg.com/@google/markerclustererplus@4.0.1/dist/markerclustererplus.min.js";
-      window.document.body.appendChild(clustererScript);
-
-      googleMapScript.addEventListener("load", () => {
-        googleMap.current = createGoogleMap();
-        // marker.current =
-        getAirportCoords();
-      });
-=======
 function GoogleMaps({ airports, updateAirportCoords, flightPath, updateFLightPath }) {
     // refs
     const googleMapRef = React.createRef();
@@ -168,16 +45,9 @@ function GoogleMaps({ airports, updateAirportCoords, flightPath, updateFLightPat
     async function getAirportCoords() {
         updateAirportCoords();
         updateFLightPath();
->>>>>>> master
     }
   }, [airports, flightPath]);
 
-<<<<<<< HEAD
-  return <div id="google-map" ref={googleMapRef} style={mapStyles} />;
-}
-
-export default GoogleMaps;
-=======
 
     // useEffect Hook
 
@@ -251,4 +121,3 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(GoogleMaps)
->>>>>>> master
