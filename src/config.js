d8c5b5182ab97eeb73f880dc4eb6e553424ac4a1
James Ballard <jamesballard901@gmail.com>
2020-06-14 18:43:18 -0500

    config var

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   api:
     process.env.NODE_ENV === "development"
       ? "http://localhost:5000"
-      : "https://flippy-flapp-api.herokuapp.com",
+      : "postgres://dmkdlrszkbkhni:2b22ec3b6d7bebf63ed71cec8344f66dfa49d79a44a3f8a1c8dda2ac8b6a09d9@ec2-34-194-198-176.compute-1.amazonaws.com:5432/dgeq2e5a46s9l",
 };
